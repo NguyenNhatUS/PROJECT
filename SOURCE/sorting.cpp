288474245ee2cc306358427e080885ddb113b703
NguyenNhatUS <nldnhat182006@gmail.com>
2025-04-02 16:57:06 +0700

    template for a function

--- SOURCE/sorting.cpp
+++ SOURCE/sorting.cpp
@@ -1,1 +1,16 @@
 // trình bày các thuật toán sort ở đây
+#include<sorting.h>
+
+void selection_sort(int a[],int n,long long& comparisions) {
+    for(int i = 0;++ comparisions && i < n - 1;i++) {
+        int index = i;
+        for(int j = i + 1;comparisions++ && j < n;j++) {
+            if(++comparisions && a[j] < a[index]) {
+                index = j;
+            }
+        }
+        int temp = a[i];
+        a[i] = a[index];
+        a[index] = temp;
+    }
+}
